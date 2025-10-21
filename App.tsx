47c7f04b4cbfed5180077dc1901043350ad031ca
Anya <143171419+its-anya@users.noreply.github.com>
2025-10-21 12:51:25 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -10,11 +10,13 @@ import CheckoutPage from './pages/CheckoutPage';
 import OrderConfirmationPage from './pages/OrderConfirmationPage';
 import TrackOrderPage from './pages/TrackOrderPage';
 import { CartProvider } from './context/CartContext';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <CartProvider>
       <HashRouter>
+        <ScrollToTop />
         <div className="flex flex-col min-h-screen bg-pink-50 text-gray-800 font-sans">
           <Header />
           <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
